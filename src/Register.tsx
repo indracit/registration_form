3d2ab87bc60c4be749882238a3c2a379bf81a613
Indracit S <indrajithij802@gmail.com>
2023-05-19 16:35:51 +0530

    end

--- src/Register.tsx
+++ src/Register.tsx
@@ -1,38 +1,68 @@
 import { FormEvent, useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './index.css';
 
+type userDetails = {
+    name : string,
+    password : string
+}
 const Register = () => {
 
-    const [username,setUsername] = useState<string | null>(null);
-    const [password,setPassword] = useState<string | null>(null);
-    const [confirmpassword,setConfirmPassword] = useState<string | null>(null);
+    const [username,setUsername] = useState<string >('');
+    const [password,setPassword] = useState<string >('');
+    const [confirmpassword,setConfirmPassword] = useState<string >('');
 
     const handleSubmit = (e : FormEvent<HTMLFormElement>)=>{
         e.preventDefault();
 
+        const data : userDetails ={
+            name:username,
+            password: password
+        }
+        console.log(data);
         
+                
     }
 
 return (
     <>
     <section className='container'>
-    <h1>Register</h1>
+        <h1>Register</h1>
 
-    <form className='form-container' onSubmit={handleSubmit}>
-    <label htmlFor='username'>Username:</label>
-    <input type="text" id='username'/>
-    <label htmlFor='password'>Password:</label>
-    <input type="password" id='password'/>
-    <label htmlFor='confirmpassword'>Confirm Password:</label>
-    <input type="text" id='confirmpassword'/>
-    <button type="submit">Sign Up</button>
-    </form>
-    <p>
-        Already Registered <br/>
-        <span>
-            <a href="#">Sign In</a>
-        </span>
-    </p>
+        <form className='form-container' onSubmit={handleSubmit}>
+            <label htmlFor='username'>Username:</label>
+            <input 
+                type="text" 
+                id='username'
+                value = {username}
+                onChange={(e)=>setUsername(e.target.value)}
+                required
+
+            />
+            <label htmlFor='password'>Password:</label>
+            <input 
+                type="password" 
+                id='password'
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+                required
+            />
+            <label htmlFor='confirmpassword'>Confirm Password:</label>
+            <input 
+                type="password" 
+                id='confirmpassword'
+                value={confirmpassword}
+                onChange={(e)=>setConfirmPassword(e.target.value)}
+                required
+            />
+            <button type="submit">Sign Up</button>
+        </form>
+        <p className='alreadyregistered'>
+            Already Registered <br/>
+            <span>
+                <a href="#">Sign In</a>
+            </span>
+        </p>
     </section>
     </>
     
